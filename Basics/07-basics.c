826c0780f1038b4e6726c53d9f57b53669df339c
salah mohamed <salahhamada63@gmail.com>
2024-02-01 06:27:26 +0200

    simple switch statement

---
+++ Basics/07-basics.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char User_Operator;
+float NumberOne, NumberTwo;
+
+int main() {
+    printf("Please enter the Operator (+, -, *, /): ");
+    scanf(" %c", &User_Operator); 
+
+    printf("Please enter the first number: ");
+    scanf("%f", &NumberOne);
+
+    printf("Please enter the second number: ");
+    scanf("%f", &NumberTwo);
+
+    switch (User_Operator) {
+    case '+':
+        printf("Result: %0.2f + %0.2f = %0.2f \n", NumberOne, NumberTwo, NumberOne + NumberTwo);
+        break;
+
+    case '-':
+        printf("Result: %0.2f - %0.2f = %0.2f \n", NumberOne, NumberTwo, NumberOne - NumberTwo);
+        break;
+
+    case '*':
+        printf("Result: %0.2f * %0.2f = %0.2f \n", NumberOne, NumberTwo, NumberOne * NumberTwo);
+        break;
+
+    case '/':
+        if (NumberTwo != 0) {
+            printf("Result: %0.2f / %0.2f = %0.2f \n", NumberOne, NumberTwo, NumberOne / NumberTwo);
+        } else {
+            printf("Error: Division by zero is undefined.\n");
+        }
+        break;
+
+    default:
+        printf("Invalid Operator !!\n");
+        break;
+    }
+
+    return 0;
+}
