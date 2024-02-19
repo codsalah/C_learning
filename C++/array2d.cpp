caf2ef78e2854319152b671b59eee9a6bc3754e6
codsalah <salahhamada63@gmail.com>
2024-02-20 01:18:23 +0200

    20/2/24

--- C++/array2d.cpp
+++ C++/array2d.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main (){
-
-
+int main() {
     const int rows = 3;
     const int columns = 3;
 
     // Good Practice
-    int array2d[rows][columns] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int array2d[rows][columns] = {{1, 2, 3},
+                                  {4, 5, 6},
+                                  {7, 8, 9}};
 
+    cout << "Using Good Practice:\n";
     cout << array2d[1][2] << "\n";  //6
     cout << array2d[2][0] << "\n";  //7
     cout << array2d[2][2] << "\n";  //9
 
+    // Bad Practice
+    int array2dd[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    // // Bad Practice
-    // int array2dd[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    cout << "\nUsing Bad Practice:\n";
+    cout << array2dd[1][2] << "\n";  //6
+    cout << array2dd[2][0] << "\n";  //7
+    cout << array2dd[2][2] << "\n";  //9
 
-    // cout << array2dd[1][2] << "\n";  //6
-    // cout << array2dd[2][0] << "\n";  //7
-    // cout << array2dd[2][2] << "\n";  //9
-
-
-
+    return 0;
 }
