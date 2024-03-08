edcd56fc3aae051424df92fe2ba9fe6970c06a30
codsalah <salahhamada63@gmail.com>
2024-03-08 23:06:14 +0200

    8/3/24

---
+++ Mastering Skills/h1triangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main(){
+    cout << "*\n";
+    cout << "**\n";
+    cout << "***\n";
+    cout << "****\n";
+    cout << "*****\n";
+
+    return 0; 
+    
+}
