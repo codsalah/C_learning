324a1fdacec6738a59a3cc0cb4335977ddf9d7fc
codsalah <salahhamada63@gmail.com>
2024-03-08 23:11:37 +0200

    8/3/24

---
+++ Mastering Skills/h2diamond.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+main(){
+    cout << "     *      \n";
+    cout << "    ***      \n";
+    cout << "   *****      \n";
+    cout << "  *******      \n";
+    cout << " *********       \n";
+    cout << "  *******      \n";
+    cout << "   *****      \n";
+    cout << "    ***      \n";
+    cout << "     *      \n";
+    return 0;
+}
+
+/*
+     *      
+    ***      
+   *****
+  *******
+ *********
+  *******
+   *****
+    ***
+     *
+*/
