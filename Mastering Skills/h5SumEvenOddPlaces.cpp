8dd8cf54acf64b78463f12fdfcfe9ab4fc143336
codsalah <salahhamada63@gmail.com>
2024-03-09 00:10:10 +0200

    9/3/24

---
+++ Mastering Skills/h5SumEvenOddPlaces.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "enter your 8 numbers: ";
+    int a, b, c, d, e, f, g, h;
+    cin >> a >> b >> c >> d >> e >> f >> g >> h ;
+    float evensum, oddsum ;
+    cout << "\neven sum is: " <<a + c + e + g; 
+    cout << "\nodd sum is: " << b +d + f + h; 
+
+}
