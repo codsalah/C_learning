85190297e51b1af33a13409f7fc1e78ea165dce6
codsalah <salahhamada63@gmail.com>
2024-03-09 21:46:44 +0200

    9/3/24

---
+++ Mastering Skills/h10.cpp
@@ -0,0 +1,17 @@
+/*
+
+Homework 3: Simplify expressions
+● For each expression:
+○ Manually Simplify it step by step to finally be a T or F
+○ Optional: Write a line of code to verify the result
+● T && T && F && T
+● T && T && F && T || T && T
+● T && T && T && T || T && (T || F)
+● T && T && T || T && (F || (T && (T && T)))
+● T && T || T && F && T || T && T && F || (T && (T || F))
+● T && T || T && F && T || (T && T && F || (T && (T || F)))
+● (T && T || T && F && T || T) && T && F || (T && (T || F))
+● T && T || T && (F && T || T && T) && F || (T && (T || F))
+
+
+*/
