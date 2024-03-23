4ea6654ea167adf21b3d332efab465535b73539b
codsalah <salahhamada63@gmail.com>
2024-03-23 09:16:15 +0200

    23/3/24

---
+++ CodeForces/R. Age in Days.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+  cout << (n/365) << " years" << endl;
+  n = n%365;
+  cout << (n/30) << " months" << endl;
+  n = n%30;
+  cout << n << " days" << endl;
+
+return 0;
+
+  
+}
