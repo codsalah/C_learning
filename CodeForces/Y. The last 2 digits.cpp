4e84452efedc7813562ccc348d4d53c335484571
codsalah <salahhamada63@gmail.com>
2024-03-23 11:26:07 +0200

    23/3/24

---
+++ CodeForces/Y. The last 2 digits.cpp
@@ -0,0 +1,15 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     long long A, B, C, D;
+//     cin >> A >> B >> C >> D;
+
+//     long long product = (A % 100) * (B % 100) * (C % 100) * (D % 100);
+
+//     int last_two_digits = product % 100;
+
+//     cout << last_two_digits << endl;
+
+//     return 0;
+// }
---
+++ CodeForces/Z. Hard Compare.cpp
@@ -0,0 +1,15 @@
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// int main() 
+// {
+//   int a,b,c,d;
+//   cin >> a>> b>> c>> d;
+//   if(b*log(a) > d*log(c))
+//     cout <<  "YES";
+//   else
+//   cout << "No"; 
+//   return 0;
+  
+// }
