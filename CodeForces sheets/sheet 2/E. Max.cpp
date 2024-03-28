d1215141bcac1cdc62d017d901ecb718cc2f41f4
codsalah <salahhamada63@gmail.com>
2024-03-28 08:33:30 +0200

    28/3/24

---
+++ CodeForces sheets/sheet 2/E. Max.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int N;
+  int maxi = 0;
+  cin >> N;
+  int num;
+  for(int i = 0 ; N > i; i++)
+  {
+    cin >> num;
+    if(num > maxi)
+      maxi = num;
+  }
+  cout << maxi << endl;
+}
