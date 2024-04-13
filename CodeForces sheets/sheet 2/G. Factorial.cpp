d4e641e9c0165dfc45e55e04bfdd45c0d8339fd9
codsalah <salahhamada63@gmail.com>
2024-04-13 20:25:01 +0200

    13/4/24

--- CodeForces sheets/sheet 2/G. Factorial.cpp
+++ CodeForces sheets/sheet 2/G. Factorial.cpp
@@ -3,5 +3,18 @@ using namespace std;
 
 int main()
 {
-    
+    int t, n;
+    cin >>t;
+    for(int i =1 ;i<=t ;i++ )
+    {
+        //take n each time 
+        cin >> n;
+        long long fact = 1;
+        for(int i=1;i<=n ; i++)
+        {
+            fact = fact*i;
+        }
+        cout << fact << endl;
+    }
+    return 0;
 }
