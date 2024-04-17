d527a415f59774b64f1a64b7b801be489ef018be
codsalah <salahhamada63@gmail.com>
2024-04-18 01:55:14 +0200

    18/4/24 fixed typos

--- CodeForces sheets/sheet 1/D. Difference.cpp
+++ CodeForces sheets/sheet 1/D. Difference.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     long long num1, num2, num3, num4;
     cin>> num1>> num2>> num3>> num4;
-    long long result = (num1*num2) * (num3*num4);
+    long long result = (num1*num2) - (num3*num4);
     cout << "Difference = " << result << endl;
 }
